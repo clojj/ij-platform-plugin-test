1552bfc50fda6dde43a5bca2eb57f95b4f3fd3d3
GitHub Action <action@github.com>
2022-01-30 09:58:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/clojj/ijplatformplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.clojj.ijplatformplugintest.services
+
+import com.github.clojj.ijplatformplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/clojj/ijplatformplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/clojj/ijplatformplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.clojj.ijplatformplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.clojj.ijplatformplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
